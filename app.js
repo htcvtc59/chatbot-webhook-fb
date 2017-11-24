3a10bbbe6948ef7ac5b94bb2d59224f839435916
htcvtc59 <htcvtc59@gmail.com>
2017-11-24 13:01:27 +0700

    messenger

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send("Hello");
 });
 
